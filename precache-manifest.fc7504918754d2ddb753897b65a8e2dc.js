18be3b7fd1fcc9a2185297ee52c70f83ca960a26
jordanwsm <63377200+jordanwsm@users.noreply.github.com>
2020-05-13 20:29:16 +0100

    Updates

---
+++ precache-manifest.fc7504918754d2ddb753897b65a8e2dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f433478adcec37238516b5fec521410",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0cc3d6033df400caee",
+    "url": "/static/css/main.f1e4bf5e.chunk.css"
+  },
+  {
+    "revision": "6aa5d3310779901dcf81",
+    "url": "/static/js/2.8b8b15cf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8b8b15cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0cc3d6033df400caee",
+    "url": "/static/js/main.85289740.chunk.js"
+  },
+  {
+    "revision": "4ce1264c592f3de7b788",
+    "url": "/static/js/runtime-main.127f70dd.js"
+  },
+  {
+    "revision": "62041c456abbf98537a7618e7703313f",
+    "url": "/static/media/imgTwo.62041c45.jpg"
+  },
+  {
+    "revision": "77718ca883054b3adb00a66a058e8d97",
+    "url": "/static/media/index.77718ca8.jpg"
+  }
+]);

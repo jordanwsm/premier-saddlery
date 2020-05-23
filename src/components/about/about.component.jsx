0f68fdc24fb8f0d074ba227c85c8c4c8d613d5d7
jordanwsm <63377200+jordanwsm@users.noreply.github.com>
2020-05-23 12:28:40 +0100

    Updated services and contacts

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import './about.style.css';
 import ImageThree from '../../assets/index.jpg';
+import ImageFour from '../../assets/saddle.JPG';
+import ImageFive from '../../assets/rug.jpg';
+import ImageSix from '../../assets/collar2.jpg';
+import ImageSeven from '../../assets/worm_egg_count.jpg';
+import ImageEight from '../../assets/sqp.jpg';
+import ImageNine from '../../assets/redmills.png';
+import ImageTen from '../../assets/gain-logo.png';
+import ImageEleven from '../../assets/spillers.JPG';
+import Image12 from '../../assets/blugrass.jpg';
+import Image13 from '../../assets/baileys.jpg';
+
+
+
+
+
 
 const About = () => {
     return (
         <div id="about">
             <div class="card">
-            
+
                 <div class="card-body">
-                <img src={ImageThree} width="200" height="100"/>
-                <p></p>
+                    <img src={ImageThree} width="200" height="100" />
+                    <p></p>
                     <h5 class="card-title">Welcome to Premier Saddlery</h5>
                     <p class="card-text">Premier Saddlery is situated 5 minutes from Armagh City and has customer service at it's core.</p>
 
@@ -19,26 +34,143 @@ const About = () => {
             </div>
 
             <div class="card">
-            
-                <div class="card-body">
-                    <h5 class="card-title">What we offer</h5>
-                    <p class="card-text">Premier Saddlery stocks a wide range of products to cater for all your equestrian needs. We supply the best quality in saddlery, riding wear, horse clothing, stable equipment, horse feed, supplements/horse care and much more. We also offer an on site saddlery repair service which affords high standards without delaying the return of your tack. The customer is at the centre of all we do.</p>
 
-
-                </div>
-            </div>
-
-
-            <div class="card">
-        
                 <div class="card-body">
                     <h5 class="card-title">General Information</h5>
                     <p class="card-text">Premier Saddlery stocks a wide range of products to cater for all your equestrian needs. We supply the best quality in saddlery, riding wear, horse clothing, stable equipment, horse feed, supplements/horse care and much more. We also offer an on site saddlery repair service which affords high standards without delaying the return of your tack. The customer is at the centre of all we do.</p>
 
+
                 </div>
             </div>
 
+            <div id="accordion">
+
+                <div class="card">
+                    <div class="card-header">
+                        <a class="card-link" data-toggle="collapse" href="#collapseOne">
+                            On Site SQP for horse wormers   </a>
+                    </div>
+                    <div id="collapseOne" class="collapse show" data-parent="#accordion">
+                        <div class="card-body">
+                        30 minutes Our in-store J-SQP is qualified to tailor worming programmes for your horse and prescribe appropriate wormers where needed - Price Varies     </div>
+                        <img src={ImageEight} width="350" height="250" />
+                        <p></p>
+                    </div>
+                </div>
+
+
+
+
+          
+
+
+  
+
+                <div class="card">
+                    <div class="card-header">
+                        <a class="card-link" data-toggle="collapse" href="#collapseTwo">
+                            Saddlery Repair</a>
+                    </div>
+                    <div id="collapseTwo" class="collapse show" data-parent="#accordion">
+                        <div class="card-body">
+                        30 minutes - On site saddlery repairs carried out - Price Varies</div>
+                        <img src={ImageFour} width="400" height="250" />
+                        <p></p>
+                    </div>
+                </div>
+
+
+
+
+
+        
+
+                <div class="card">
+                    <div class="card-header">
+                        <a class="card-link" data-toggle="collapse" href="#collapseThree">
+                            Rug Wash Repair</a>
+                    </div>
+                    <div id="collapseThree" class="collapse show" data-parent="#accordion">
+                        <div class="card-body">
+                        30 minutes - Comprehensive professional service with minimum turnaround - £10</div>
+                        <img src={ImageFive} width="400" height="250" />
+                        <p></p>
+                    </div>
+                </div>
+
+        
+
+                <div class="card">
+                    <div class="card-header">
+                        <a class="card-link" data-toggle="collapse" href="#collapseFour">
+                            Faecal Work Egg Counts </a>
+                    </div>
+                    <div id="collapseFour" class="collapse show" data-parent="#accordion">
+                        <div class="card-body">
+                        30 minutes - Worm egg counts available to purchase in store. Professional and speedy service to determine if your horse needs wormed or not! Save money and reduce resistance build up! Only £9.99
+                            </div>
+                            <img src={ImageSeven} width="400" height="250" />
+                        <p></p>
+                    </div>
+                </div>
+
+
+                <div class="card">
+                    <div class="card-header">
+                        <a class="card-link" data-toggle="collapse" href="#collapseFive">
+                            Feed Stock List </a>
+                    </div>
+                    <div id="collapseFive" class="collapse show" data-parent="#accordion">
+                        <div class="card-body">
+                      Red Mills | Gain Horse Feeds | Spillers | Bluegrass | Baileys Horse Feed
+                          
+
+                            
+                            
+            </div>
+
+            <div class = "Row">
+
+            <img src={ImageNine} width="325" height="250" />
+            <img src={ImageTen} width="325" height="250" />
+            <img src={ImageEleven} width="375" height="250" />
+            <img src={Image12} width="375" height="250" />
+            <img src={Image13} width="325" height="250" />
+
+
+            </div>
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+            <div id="accordion">
+
+<div class="card">
+    <div class="card-header">
+        <a class="card-link" data-toggle="collapse" href="#collapseSix">
+            Supplements </a>
+    </div>
+    <div id="collapseSix" class="collapse show" data-parent="#accordion">
+        <div class="card-body">
+      NAF | Horse First | EquireAmerian
+            
+</div>
+
+    </div>
+</div>
+
+
+
+
+</div>
+
+
         </div>
+
 
 
 
--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+import {  } from "@fortawesome/free-brands-svg-icons"
 import './contact.style.css';
 
 
@@ -9,18 +12,18 @@ const Contact = () => {
 
 
         <div id="contact">
-           
-            
+
+
             <div class="card">
-            
+
                 <div class="card-body">
                     <h5 class="card-title">Standard Opening Hours</h5>
                     <p class="card-text"> Due to Covid-19 the shop will be closing at 4.00pm each day</p>
-                    <p class="card-text"> Monday - 9.00am 5.30pm </p>
-                    <p class="card-text"> Tuesday - 9.00am 5.30pm </p>
-                    <p class="card-text"> Wednesday - 9.00am 5.30pm </p>
-                    <p class="card-text"> Thursday - 9.00am 5.30pm </p>
-                    <p class="card-text"> Friday - 9.00am 5.30pm </p>
+                    <p class="card-text"> Monday - 9.00am 6.00pm </p>
+                    <p class="card-text"> Tuesday - 9.00am 6.00pm </p>
+                    <p class="card-text"> Wednesday - 9.00am 6.00pm </p>
+                    <p class="card-text"> Thursday - 9.00am 6.00pm </p>
+                    <p class="card-text"> Friday - 9.00am 6.00pm </p>
                     <p class="card-text"> Saturday - 9.00am 4.00pm </p>
                     <p class="card-text"> Sunday - Closed </p>
 
@@ -28,20 +31,26 @@ const Contact = () => {
                 </div>
             </div>
 
- 
+
             <div class="card">
-            
-            <div class="card-body">
-                <h5 class="card-title">Contact Information</h5>
-                <p class="card-text"> Email - premiersaddlery@gmail.com </p>
-                <p class="card-text"> Landline - 028 3752 5492</p>
-                <p class="card-text"> Address - 17 Terraskane Rd, Armagh, BT61 8BJ</p>
-                <p class="card-text"> Facebook - Premier Saddlery</p>      
+
+                <div class="card-body">
+                    <h5 class="card-title">Contact Information</h5>
+                    <p class="card-text"> Email - premiersaddlery@gmail.com </p>
+                    <p class="card-text"> Landline - 028 3752 5492</p>
+                    <p class="card-text"> Address - 17 Terraskane Rd, Armagh, BT61 8BJ</p>
+                    <a class="card-text" href="https://www.facebook.com/premiersaddlery"> <FontAwesomeIcon icon={faFacebook}/> Facebook - Premier Saddlery</a>
+                </div>
             </div>
-        </div>
+
+            <div class="mapouter">
+                <div class="gmap_canvas">
+                    <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=PREMIER%20SADDLery&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                        </iframe>
+                        </div>
 
         </div>
-
+</div>
 
 
     )
